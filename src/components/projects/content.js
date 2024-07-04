4b99695e30cbd0da6e8537be159f564c75971119
Suleymanova-Gamar <suleymanovagamar03@gmail.com>
2024-07-04 20:08:28 +0400

    adding duplicated project for testing slider

--- src/components/projects/content.js
+++ src/components/projects/content.js
@@ -52,6 +52,49 @@ const content = [
         codesLink: '',
         designLink: ''
     },
+
+    {
+        imgSrc: {
+            laptopSize: img1,
+            tabletSize: img2,
+            mobileSize: img3
+        },
+        imgAlt: 'Screenshot of the Helply website',
+        liveLink: 'https://helply.ae/',
+        info: {
+            header: 'Helply',
+            descriptions: "The goal of the Helply is to provide users with a personalized therapists match tailored to their preferences and needs.",
+            tools: [
+                {
+                    toolName: 'HTML',
+                    toolIcon: faHtml5,
+                    iconColor: '#F08959'
+                },
+                {
+                    toolName: 'CSS',
+                    toolIcon: faCss3Alt,
+                    iconColor: '#51709F'
+                },
+                {
+                    toolName: 'JS',
+                    toolIcon: faSquareJs,
+                    iconColor: '#EDD858'
+                },
+                {
+                    toolName: 'Boostrap',
+                    toolIcon: faBootstrap,
+                    iconColor: '#a020f0'
+                },
+                {
+                    toolName: 'JQuery',
+                    toolIcon: faJ,
+                    iconColor: '#0769ad'
+                }
+            ]
+        },
+        codesLink: '',
+        designLink: ''
+    },
 ];
 
 export default content;
